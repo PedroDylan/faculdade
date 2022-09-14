a2e815bebb1d2721c7361f7fc8c859a48197a80d
pedro <pedrodylan3@gmail.com>
2022-09-14 18:13:15 -0300

    resolvendo a questão 6

---
+++ lista_03/q06.py
@@ -0,0 +1,34 @@
+notas_classe = []
+for i in range(10):
+    notas_aluno = []
+    for i in range(3):
+        x = float(input("Insira uma nota semestral: "))
+        notas_aluno.append(x)
+    notas_classe.append(notas_aluno)
+
+
+def calcula_media_aluno(lista):
+    sum = 0 
+    for x in lista:
+        sum += x
+    return sum/3
+
+def calcula_media_turma(lista):
+    sum = 0
+    for x in lista:
+        sum += calcula_media_aluno(x)
+    return sum/10
+      
+def situacao_acad(lista):
+    if calcula_media_aluno(lista) >= 7:
+        return "Aprovado"
+    elif 4 <= calcula_media_aluno(lista) <= 6.9:
+        return "AF"
+    else:
+        return "Reprovado"
+
+
+print(notas_classe)
+print(calcula_media_turma(notas_classe))
+for x in notas_classe:
+    print(situacao_acad(x))
