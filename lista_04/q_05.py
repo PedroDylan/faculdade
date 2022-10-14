944c074759f26aa76b43135021a7ae30c68deb63
Pedro <pedrodylan3@gmail.com>
2022-10-14 09:07:16 -0300

    corrigindo a funcionalidade de receber input da 5

--- lista_04/q_05.py
+++ lista_04/q_05.py
@@ -7,24 +7,33 @@ class Funcionario():
         self.apto = False
 
     def aposentadoria(self):
-        if self.sexo == "Homem" and self.idade >= 65 and self.trabalho >= 35:
+        if self.sexo == "homem" and self.idade >= 65 and self.trabalho >= 35:
             self.apto = True
-        elif self.sexo == "Mulher" and self.idade >= 60 and self.trabalho >= 30:
+        elif self.sexo == "mulher" and self.idade >= 60 and self.trabalho >= 30:
             self.apto = True
         else:
             self.apto = False
 
         if self.apto:
-            print("{} apto para aposentadoria".format(self.nome))
+            return("{} apto para aposentadoria".format(self.nome))
         else: 
-            print("{} não apto para aposentadoria".format(self.nome))
+            return("{} não apto para aposentadoria".format(self.nome))
 
+num_func = 6
+lista_func = []
+for i in range(num_func):
+    nome = str(input("Insira o nome: "))
+    idade = int(input("Insira a idade: "))
+    sexo = str(input("Insira o sexo: "))
+    trabalho = int(input("insira o número de anos trabalhados: "))
 
-        
+    jon_doe = Funcionario(nome,idade,sexo,trabalho)
+    lista_func.append(jon_doe) 
 
-joao = Funcionario("Joao", 85, "Homem", 40)
-marcelo = Funcionario("Marcelo", 40, "Homem", 20)
-carla = Funcionario("Carla", 70, "Mulher", 45)
-joao.aposentadoria()
-carla.aposentadoria()
-marcelo.aposentadoria()
+for x in lista_func:
+    print(x.nome)
+    print(x.idade)
+    print(x.sexo)
+    print(x.trabalho)
+    print(x.aposentadoria())
+    
